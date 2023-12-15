015cb66fb9652cfde48c1534b6676e6d90d857e4
jrobi015 <jamerobinson@deloitte.com>
2023-12-15 13:47:32 -0500

    api integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // fetch("https://23oerh0rlk.execute-api.us-east-1.amazonaws.com/test", {
+    //   method: "GET",
+    //   mode: 'no-cors',
+    // })
+    axios.get('https://23oerh0rlk.execute-api.us-east-1.amazonaws.com/test')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
