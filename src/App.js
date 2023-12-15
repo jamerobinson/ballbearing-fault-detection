0ce7164d629fcc175b6d669cefd2e80091659788
jrobi015 <jamerobinson@deloitte.com>
2023-12-15 13:56:59 -0500

    add text

--- src/App.js
+++ src/App.js
@@ -17,18 +17,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. This is a test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Ballbearing Fault Detection App
       </header>
     </div>
   );
